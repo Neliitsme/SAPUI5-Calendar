beac2a9ef4c40b5a10d79b8e40c210a730630666
Nelite <neliitsme14@gmail.com>
2022-06-07 18:42:53 +0300

    feat: add completed walkthrough up to step 18 (incl.)

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,31 +1,41 @@
-sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "sap/ui/Device",
-        "sapui5calendar/model/models"
-    ],
-    function (UIComponent, Device, models) {
-        "use strict";
+sap.ui.define(
+  [
+    'sap/ui/core/UIComponent',
+    'sap/ui/Device',
+    'sapui5calendar/model/models',
+    'sap/ui/model/json/JSONModel',
+  ],
+  function (UIComponent, Device, models, JSONModel) {
+    'use strict';
 
-        return UIComponent.extend("sapui5calendar.Component", {
-            metadata: {
-                manifest: "json"
-            },
+    return UIComponent.extend('sapui5calendar.Component', {
+      metadata: {
+        interfaces: ['sap.ui.core.IAsyncContentCreation'],
+        manifest: 'json',
+      },
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
+      /**
+       * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+       * @public
+       * @override
+       */
+      init: function () {
+        // call the base component's init function
+        UIComponent.prototype.init.apply(this, arguments);
+        var oData = {
+          recipient: {
+            name: 'World',
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.setModel(oModel);
 
-                // enable routing
-                this.getRouter().initialize();
+        // enable routing
+        this.getRouter().initialize();
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
-            }
-        });
-    }
+        // set the device model
+        this.setModel(models.createDeviceModel(), 'device');
+      },
+    });
+  },
 );
--- webapp/controller/Calendar.controller.js
+++ webapp/controller/Calendar.controller.js
@@ -1,15 +1,39 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ['sap/ui/core/mvc/Controller', 'sap/m/MessageToast', 'sap/ui/core/Fragment'],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast, Fragment) {
+    'use strict';
 
-        return Controller.extend("sapui5calendar.controller.Calendar", {
-            onInit: function () {
+    return Controller.extend('sapui5calendar.controller.Calendar', {
+      onInit: function () {},
 
-            }
+      onOpenDialog: function () {
+        // Create dialog lazily
+        if (!this.pDialog) {
+          this.pDialog = this.loadFragment({
+            name: 'sapui5calendar.view.HelloDialog',
+          });
+        }
+        this.pDialog.then((oDialog) => {
+          oDialog.open();
         });
+      },
+
+      onCloseDialog: function () {
+        this.byId('helloDialog').close();
+      },
+
+      onShowWorld: function () {
+        var oBundle = this.getView().getModel('i18n').getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty('/recipient/name');
+        var sMsg = oBundle.getText('helloMsg', [sRecipient]);
+
+        MessageToast.show(sMsg);
+      },
     });
+  },
+);

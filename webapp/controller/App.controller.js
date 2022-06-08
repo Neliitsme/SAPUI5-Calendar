f2b163d234b6844e247f0671b914dd73853af296
Nelite <neliitsme14@gmail.com>
2022-06-08 20:00:33 +0300

    feat: add calendar view and theme switch

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,31 @@
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller'],
-  function (Controller) {
+  ['sap/ui/core/mvc/Controller', 'sap/m/Switch', 'sap/ui/core/library'],
+  function (Controller, Switch) {
     'use strict';
 
-    return Controller.extend('sapui5calendar.controller.App', {});
-    
+    return Controller.extend('sapui5calendar.controller.App', {
+      onInit: function () {
+        var bSwitchState = localStorage.getItem('themeSwitchState') === 'true';
+        var oSwitch = this.byId('themeSwitch');
+        oSwitch.setState(bSwitchState);
+        this._handleThemeChange(bSwitchState);
+      },
+      handleThemeSwitch: function (oEvent) {
+        var oSwitch = oEvent.getSource();
+        var bSwitchState = oSwitch.getState();
+        this._handleThemeChange(bSwitchState);
+        localStorage.setItem('themeSwitchState', bSwitchState);
+      },
+
+      // true === dark, false === light
+      _handleThemeChange: function (bSwitchState) {
+        var oCore = sap.ui.getCore();
+        if (bSwitchState) {
+          oCore.applyTheme('sap_fiori_3_dark');
+        } else {
+          oCore.applyTheme('sap_fiori_3');
+        }
+      },
+    });
   },
 );
--- webapp/controller/Calendar.controller.js
+++ webapp/controller/Calendar.controller.js
@@ -1,38 +1,100 @@
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller', 'sap/m/MessageToast', 'sap/ui/core/Fragment'],
+  [
+    'sap/ui/core/mvc/Controller',
+    'sap/m/MessageToast',
+    'sap/ui/core/Fragment',
+    'sap/ui/unified/DateRange',
+    'sap/ui/core/format/DateFormat',
+    'sap/ui/core/library',
+  ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, MessageToast, Fragment) {
+  function (
+    Controller,
+    MessageToast,
+    Fragment,
+    DateRange,
+    DateFormat,
+    coreLibrary,
+  ) {
     'use strict';
+    var CalendarType = coreLibrary.CalendarType;
 
     return Controller.extend('sapui5calendar.controller.Calendar', {
-      onInit: function () {},
-
-      onOpenDialog: function () {
-        // Create dialog lazily
-        if (!this.pDialog) {
-          this.pDialog = this.loadFragment({
-            name: 'sapui5calendar.view.HelloDialog',
-          });
-        }
-        this.pDialog.then((oDialog) => {
-          oDialog.open();
+      oFormatYyyymmdd: null,
+      onInit: function () {
+        this.oFormatYyyymmdd = DateFormat.getInstance({
+          pattern: 'yyyy-MM-dd',
+          calendarType: CalendarType.Gregorian,
         });
       },
 
-      onCloseDialog: function () {
-        this.byId('helloDialog').close();
+      handleCalendarSelect: function (oEvent) {
+        var oCalendar = oEvent.getSource();
+        this._updateText(oCalendar.getSelectedDates()[0]);
       },
 
-      onShowWorld: function () {
-        var oBundle = this.getView().getModel('i18n').getResourceBundle();
-        var sRecipient = this.getView()
-          .getModel()
-          .getProperty('/recipient/name');
-        var sMsg = oBundle.getText('helloMsg', [sRecipient]);
+      _updateText: function (oSelectedDates) {
+        var oSelectedDateFrom = this.byId('selectedDateFrom'),
+          oSelectedDateTo = this.byId('selectedDateTo'),
+          oDate;
 
-        MessageToast.show(sMsg);
+        if (oSelectedDates) {
+          oDate = oSelectedDates.getStartDate();
+          if (oDate) {
+            oSelectedDateFrom.setText(this.oFormatYyyymmdd.format(oDate));
+          } else {
+            oSelectedDateTo.setText('No Date Selected');
+          }
+          oDate = oSelectedDates.getEndDate();
+          if (oDate) {
+            oSelectedDateTo.setText(this.oFormatYyyymmdd.format(oDate));
+          } else {
+            oSelectedDateTo.setText('No Date Selected');
+          }
+        } else {
+          oSelectedDateFrom.setText('No Date Selected');
+          oSelectedDateTo.setText('No Date Selected');
+        }
+      },
+
+      handleSelectThisWeek: function () {
+        this._selectWeekInterval(6);
+      },
+
+      handleSelectWorkWeek: function () {
+        this._selectWeekInterval(4);
+      },
+
+      handleWeekNumberSelect: function (oEvent) {
+        var oDateRange = oEvent.getParameter('weekDays'),
+          iWeekNumber = oEvent.getParameter('weekNumber');
+
+        // if (iWeekNumber % 5 === 0) {
+        //   oEvent.preventDefault();
+        //   MessageToast.show(
+        //     'You are not allowed to select this calendar week!',
+        //   );
+        //   return;
+        // }
+        this._updateText(oDateRange);
+      },
+
+      _selectWeekInterval: function (iDays) {
+        var oCurrent = new Date(), // get current date
+          iWeekStart = oCurrent.getDate() - oCurrent.getDay() + 1,
+          iWeekEnd = iWeekStart + iDays, // end day is the first day + 6
+          oMonday = new Date(oCurrent.setDate(iWeekStart)),
+          oSunday = new Date(oCurrent.setDate(iWeekEnd)),
+          oCalendar = this.byId('calendar');
+
+        oCalendar.removeAllSelectedDates();
+        oCalendar.addSelectedDate(
+          new DateRange({ startDate: oMonday, endDate: oSunday }),
+        );
+
+        this._updateText(oCalendar.getSelectedDates()[0]);
       },
     });
   },
